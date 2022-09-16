1356041d8b9e861fafafdb24846d7b126bc0ea43
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-09-16 20:47:07 -0300

    text update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Dashboard', url: '/folder/Dashboard', icon: 'mail' },
-    { title: 'Metas', url: '/folder/Metas', icon: 'paper-plane' },
+    { title: 'Dashboard', url: '/folder/Dashboard', icon: 'bar-chart' },
+    { title: 'Metas', url: '/folder/Metas', icon: 'barbell' },
     { title: 'Saúde', url: '/folder/Saude', icon: 'heart' },
-    { title: 'Blogs', url: '/folder/Blogs', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Blogs', url: '/folder/Blogs', icon: 'newspaper' },
+    // { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    // { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['beba 2L de água por dia', 'Coma de 3 horas e 3 hora', '16:30 tomar remedia'];
   constructor() {}
 }
