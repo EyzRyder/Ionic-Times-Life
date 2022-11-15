4b2a2233012257ac4bbbbfafbcbdb585a80f8eb0
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-11-15 13:52:54 -0300

    update login

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserAuthService } from '../../services/user-auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public userAuthService: UserAuthService,
+  ) { }
 
   ngOnInit() {
   }
