c43197f41839afef9377618131ea65a302f21af5
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-11-20 21:50:23 -0300

    fix log in

--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { UserAuthService } from '../../services/user-auth.service';
+import { TabsPage } from './../../tabs/tabs.page';
+
 @Component({
   selector: 'app-config',
   templateUrl: './config.page.html',
@@ -8,13 +10,18 @@ import { UserAuthService } from '../../services/user-auth.service';
 })
 export class ConfigPage implements OnInit {
 
+  selectedPage: string;
+
   constructor(
-    public appComponent: AppComponent,
+    public tabsPage: TabsPage,
     public userAuthService: UserAuthService,
+  ) { 
+    this.selectedPage = 'user';
+  }
 
-
-  ) { }
-
+  segmentChange(e: any) {
+    this.selectedPage = e.target.value;
+  }
   ngOnInit() {
   }
 
