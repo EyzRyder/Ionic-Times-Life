6902c9ae23a465b3d3d611efd9700932d3f1119a
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-11-26 19:31:35 -0300

    more stuff

--- src/app/pages/saude/saude.page.ts
+++ src/app/pages/saude/saude.page.ts
@@ -1,3 +1,4 @@
+import { TabsPage } from './../../tabs/tabs.page';
 import { ListService } from './../../services/list.service';
 import { Component, OnInit } from '@angular/core';
 import { UserAuthService } from '../../services/user-auth.service';
@@ -9,9 +10,10 @@ import { UserAuthService } from '../../services/user-auth.service';
 export class SaudePage implements OnInit {
 
   list;
-  
+
   constructor(
     public userAuthService: UserAuthService,
+    public tabsPage: TabsPage,
     public listService: ListService,
   ) {
 
@@ -20,9 +22,25 @@ export class SaudePage implements OnInit {
 
   ngOnInit() {
 
-
   }
-
+  showImcDescription(des) {
+    if (this.tabsPage.imc < 20) {
+      return this.listService.list.pesoBaixo[des];
+    }
+    else if (this.tabsPage.imc <= 25) {
+      return this.listService.list.pesoNormal[des];
+    }
+    else if (this.tabsPage.imc <= 30) {
+      return this.listService.list.preObesidade[des];
+    }
+    else if (this.tabsPage.imc <= 40) {
+      return this.listService.list.obesidade[des];
+    } else if (this.tabsPage.imc > 40) {
+      return this.listService.list.obesidadeGrave[des];
+    } else {
+      return 'Calcule seu peso no Dashboard';
+    }
+  }
 
   }
 
--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -8,17 +8,41 @@ import { Injectable } from '@angular/core';
 })
 export class ListService {
 
-  list;
+  list = {
+    pesoBaixo: {
+      nome: 'Peso Baixo',
+      descrition: 'Segundo o cálculo do IMC, o cálculo entre o seu peso e altura é abaixo do recomendado. Este valor pode ser um indicativo de alguma carência nutricional.',
+    },
+    pesoNormal: {
+      nome: 'Peso Normal',
+      descrition: 'De acordo com o cálculo do IMC, o seu peso está dentro da normalidade.Mantenha - o, seguindo uma dieta adequada e a praticando exercício físico com alguma regularidade.',
+    },
+    preObesidade: {
+      nome: 'Pré obesidade',
+      descrition: 'Tem algum excesso de peso em relação à sua altura. Comece por praticar exercício físico adequado, e melhorar os seus hábitos alimentares. A mudança está nas suas mãos!.',
+    },
+    obesidade: {
+      nome: 'Obesidade',
+      descrition: 'De acordo com o seu IMC, tem excesso de peso – obesidade. Consulte o seu médico de família para iniciar uma programa de perda de peso e melhorar a sua saúde.',
+    },
+    obesidadeGrave: {
+      nome: 'Obesidade Grave',
+      descrition: 'O seu peso é índice de obesidade mórbida, o que poderá trazer agravamentos à sua saúde. Consulte o seu médico para conseguir ajuda.',
+    }
+  };
   listDoc;
   // public listObservable: BehaviorSubject<any>;
 
 
   constructor(
+    
     // public auth: AngularFireAuth,
     // public db: AngularFirestore
-  ) { 
+  ) {
     // this.listObservable = new BehaviorSubject(null);
   }
+
+
 
   // real time collection data
 //   onSnapshot(q, (snapshot)=> {
