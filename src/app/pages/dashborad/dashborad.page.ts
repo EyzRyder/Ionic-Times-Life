40b3a72d78af389a47e02952d00cca8286773b62
yl0922 <carlosmaletti998@gmail.com>
2022-11-28 19:57:11 -0300

    Google Fit Api

--- src/app/pages/dashborad/dashborad.page.ts
+++ src/app/pages/dashborad/dashborad.page.ts
@@ -1,9 +1,11 @@
 import { UserAuthService } from './../../services/user-auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController, ToastController } from '@ionic/angular';
-import { TabsPage } from './../../tabs/tabs.page'; 
-
+import { AlertController, Platform, ToastController } from '@ionic/angular';
+import { TabsPage } from './../../tabs/tabs.page';
+import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
+import { Health } from '@awesome-cordova-plugins/health/ngx';
+import { DatePipe, formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-dashborad',
@@ -11,6 +13,9 @@ import { TabsPage } from './../../tabs/tabs.page';
   styleUrls: ['./dashborad.page.scss'],
 })
 export class DashboradPage implements OnInit {
+  platformReady: boolean = false;
+  hasData: boolean = true;
+
   nome;
   idade;
   peso;
@@ -19,17 +24,113 @@ export class DashboradPage implements OnInit {
   dataNasc;
   imc;
 
-  
+  dailySteps = [];
+  weeklySteps = [];
+  monthlySteps = [];
+
+  requestedPermissions: string[] = [
+    'android.permission.ACTIVITY_RECOGNITION',
+    'android.permission.BODY_SENSORS',
+    'android.permission.ACCESS_FINE_LOCATION',
+  ];
+
+  dataTypes: string[] = ['steps'];
+
   constructor(
+    private platform: Platform,
     public toastController: ToastController,
     public alertController: AlertController,
     public tabsPage: TabsPage,
-    public userAuthService: UserAuthService,
-    public router: Router
+    public userAuthService: UserAuthService, 
+    public androidPermissions: AndroidPermissions, 
+    public health: Health,
+    public router: Router,
   ) {
+    this.requestPermissions();
+    this.requestAuthorization();
   }
 
   ngOnInit() {
+  }
+
+  requestPermissions() {
+    this.requestedPermissions.forEach((permission) => {
+      this.androidPermissions.requestPermission(permission);
+    });
+  }
+  
+  async requestAuthorization() {
+    const ready = !!await this.platform.ready();
+    if (ready) {
+      this.health.isAuthorized(this.dataTypes).then(() => {
+        this.health.requestAuthorization(this.dataTypes).then(() => {
+          this.getDailySteps();
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  }
+
+  getDailySteps() {
+    this.health.query({
+      startDate: new Date(new Date().setHours(0, 0, 0, 0)),
+      endDate: new Date(),
+      dataType: 'steps',
+      limit: 1000,
+      filtered: true
+    }).then(arr => {
+      arr.forEach(obj => {
+        console.log(obj.value);
+        let datePipe: DatePipe = new DatePipe('en-US');
+        this.dailySteps.push({startDate: datePipe.transform(obj.startDate, 'HH:mm'), endDate: datePipe.transform(obj.endDate, 'HH:mm'), value: obj.value});
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  getWeeklySteps() {
+    this.health.query({
+      startDate: this.setToMonday(new Date()),
+      endDate: this.setToFriday(new Date()),
+      dataType: 'steps',
+      limit: 1000,
+      filtered: true
+    }).then(arr => {
+      arr.forEach(obj => {
+        this.weeklySteps.push({startDate: obj.startDate.toISOString(), endDate: obj.endDate.toISOString(), value: obj.value});
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  setToMonday(date: Date) {
+    let day = date.getDay() || 7;
+    if (day !== 1) date.setHours(-24 * (day - 1));
+    return date; 
+  }
+
+  setToFriday(date: Date) {
+    let day = date.getDay() || 7;
+    if (day !== 7) date.setHours(24 * (day + 1));
+    return date;
+  }
+
+  getUserSteps(startDate: Date, endDate: Date) {
+    this.health.query
+  }
+
+  getUserDailySteps() {
+
+  }
+
+  query() {
+
+  }
+
+  queryAggregated() {
 
   }
 
@@ -42,21 +143,21 @@ export class DashboradPage implements OnInit {
           type: 'number',
           placeholder: 'Peso em Kg',
           max: 150,
-          min: 30
+          min: 30,
         },
         {
           name: 'inputAltura',
           type: 'number',
           placeholder: 'Altura em M',
           max: 220,
-          min: 100
+          min: 100,
         },
         {
           name: 'inputNascimento',
           type: 'date',
           max: '2022-11-26',
-          min: '1900-01-01'
-        }
+          min: '1900-01-01',
+        },
       ],
       buttons: [
         {
@@ -68,37 +169,45 @@ export class DashboradPage implements OnInit {
 
             this.altura = valor['inputAltura'];
 
-            if (parseFloat((parseFloat(this.peso) / (parseFloat(this.altura) * parseFloat(this.altura))).toFixed(1)) >= 40) {
-              this.alertaSimples('Atenção', 'Você está obeso', 'MELHORE A SUA SAÚDE!');
+            if (
+              parseFloat(
+                (
+                  parseFloat(this.peso) /
+                  (parseFloat(this.altura) * parseFloat(this.altura))
+                ).toFixed(1)
+              ) >= 40
+            ) {
+              this.alertaSimples(
+                'Atenção',
+                'Você está obeso',
+                'MELHORE A SUA SAÚDE!'
+              );
             }
 
             this.imc = (this.peso / (this.altura * this.altura)).toFixed(1);
-          
-            this.exibirToast('Obrigado por responder', 'success');
 
-          }
-        }
-      ]
+            this.exibirToast('Obrigado por responder', 'success');
+          },
+        },
+      ],
     });
     await alert.present();
   }
-
 
   async alertaEntrada() {
     const alert = await this.alertController.create({
       header: 'Selecione seu Gênero',
       inputs: [
-
         {
           type: 'radio',
-          label: "Masculino",
-          value: 'M'
+          label: 'Masculino',
+          value: 'M',
         },
         {
           type: 'radio',
-          label: "Feminina",
-          value: 'F'
-        }
+          label: 'Feminina',
+          value: 'F',
+        },
       ],
 
       buttons: [
@@ -108,9 +217,9 @@ export class DashboradPage implements OnInit {
             this.sexo = valor;
             this.registrarUser();
             // console.log(this.sexo);
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
 
     await alert.present();
@@ -121,7 +230,7 @@ export class DashboradPage implements OnInit {
       header: titulo,
       subHeader: subtitulo,
       message: message,
-      buttons: ['ok']
+      buttons: ['ok'],
     });
 
     await alert.present();
@@ -131,14 +240,14 @@ export class DashboradPage implements OnInit {
     const toast = await this.toastController.create({
       message: msg,
       duration: 2000,
-      color: color
+      color: color,
     });
     toast.present();
   }
 
   showImc() {
     if (this.userAuthService.isLoggedIn == true) {
-      return this.imc = this.tabsPage.imc;
+      return (this.imc = this.tabsPage.imc);
     } else {
       return this.imc;
     }
@@ -146,22 +255,17 @@ export class DashboradPage implements OnInit {
 
   showPesoInfo() {
     if (this.imc < 20) {
-      return
-    }
-    else if (this.imc <= 25) {
+      return;
+    } else if (this.imc <= 25) {
       // this.userAuthService.getPesoInfo()
       // console.log(this.userAuthService.getPesoInfo());
-      return
-      
-    }
-    else if (this.imc <= 30) {
-      return
-    }
-    else if (this.imc <= 40) {
-      return
+      return;
+    } else if (this.imc <= 30) {
+      return;
+    } else if (this.imc <= 40) {
+      return;
     } else {
-      return
+      return;
     }
   }
-
 }
