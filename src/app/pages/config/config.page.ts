1329ce27a1880aa31b89560dbc8340951f446b48
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-11-30 23:31:45 -0300

    imc update in config

--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -1,3 +1,4 @@
+import { AlertController, ToastController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { UserAuthService } from '../../services/user-auth.service';
@@ -9,12 +10,18 @@ import { TabsPage } from './../../tabs/tabs.page';
   styleUrls: ['./config.page.scss'],
 })
 export class ConfigPage implements OnInit {
-
+  sexo;
+  altura;
+  peso;
+  imc;
+  dataNasc;
   selectedPage: string;
 
   constructor(
     public tabsPage: TabsPage,
     public userAuthService: UserAuthService,
+    public alertController: AlertController,
+    public toastController: ToastController,
   ) { 
     this.selectedPage = 'user';
   }
@@ -23,6 +30,81 @@ export class ConfigPage implements OnInit {
     this.selectedPage = e.target.value;
   }
   ngOnInit() {
+  }
+
+  async alertaSimples(titulo, subtitulo, message) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      subHeader: subtitulo,
+      message: message,
+      buttons: ['ok'],
+    });
+
+    await alert.present();
+  }
+
+  async exibirToast(msg, color) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      color: color,
+    });
+    toast.present();
+  }
+
+
+  async calcImc() {
+    const alert = await this.alertController.create({
+      header: 'Insere seus dados',
+      inputs: [
+        {
+          name: 'inputPeso',
+          type: 'number',
+          placeholder: 'Peso em Kg',
+          max: 150,
+          min: 30,
+        },
+        {
+          name: 'inputAltura',
+          type: 'number',
+          placeholder: 'Altura em M',
+          max: 220,
+          min: 100,
+        }
+      ],
+      buttons: [
+        {
+          text: 'ok',
+          handler: (valor: any) => {
+            this.altura = valor['inputAltura'];
+            this.peso = valor['inputPeso'];
+
+
+            if (
+              parseFloat(
+                (
+                  parseFloat(this.peso) /
+                  (parseFloat(this.altura) * parseFloat(this.altura))
+                ).toFixed(1)
+              ) >= 40
+            ) {
+              this.alertaSimples(
+                'Atenção',
+                'Você está obeso',
+                'MELHORE A SUA SAÚDE!'
+              );
+            }
+
+            this.userAuthService.IMC(this.tabsPage.user.id, this.tabsPage.user, this.altura, this.peso)
+
+            this.imc = (this.peso / (this.altura * this.altura)).toFixed(1);
+
+            this.exibirToast('Obrigado por responder', 'success');
+          },
+        },
+      ],
+    });
+    await alert.present();
   }
 
 }
